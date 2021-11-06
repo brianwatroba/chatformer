a704656183487482b6604bf6a0268401a69919f6
Brian Watroba <brianwatroba@gmail.com>
2021-11-06 17:53:39 -0400

    mostly done

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -1,6 +1,9 @@
 import Phaser from '../lib/phaser.js';
 
-const COLOR_PRIMARY = 0x4e342e;
+const BACKGROUND_COLOR = '#A8E9FF';
+const ERROR_COLOR = '#EF233C';
+const SUBTEXT_COLOR = '#616161';
+const CJ_PINK = '#DB79BA';
 const COLOR_LIGHT = 0x7b5e57;
 const COLOR_DARK = 0x260e04;
 
@@ -10,23 +13,59 @@ export default class StartScreen extends Phaser.Scene {
 	}
 
 	status;
+	logoSubtext;
 	message;
 	streamer;
 	nameInput;
 	client;
 	playButton;
+	platforms;
+	rect;
 
 	preload() {
 		this.load.html('form', 'scripts/input.html');
+		this.load.image('logo', 'assets/logo.png');
+		this.load.image('ground', 'assets/ground.png');
+		this.load.image('dirt', 'assets/dirt.png');
 	}
 	create() {
-		this.nameInput = this.add.dom(230, 250).createFromCache('form');
-		this.playButton = this.add.text(350, 400, 'PLAY', {
-			fill: '#DB79BA',
+		const screenCenterX =
+			this.cameras.main.worldView.x + this.cameras.main.width / 2;
+		const screenCenterY =
+			this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+		this.add.image(375, 100, 'logo').setScale(0.7);
+		for (let i = 0; i < 20; i++) {
+			this.add.image(-300 + i * 44 * 1.5, 592, 'ground').setScale(1.5);
+		}
+		this.rect = this.add.graphics();
+		this.rect.fillRoundedRect(315, 400, 160, 55, 12);
+		this.rect.fillStyle(0xff00ff, 1);
+
+		this.cameras.main.setBackgroundColor(BACKGROUND_COLOR);
+		this.nameInput = this.add.dom(235, 360).createFromCache('form');
+		this.playButton = this.add.text(357, 410, 'PLAY', {
+			fill: '#ffffff',
 			fontSize: '32px',
+			fontFamily: 'arial',
 		});
-		this.status = this.add.text(300, 100, 'Enter streamer!');
+
+		this.status = this.add
+			.text(screenCenterX, 292, 'streamer twitch handle:', {
+				color: SUBTEXT_COLOR,
+				fontSize: '14px',
+				fontFamily: 'arial',
+			})
+			.setOrigin(0.5);
 		this.playButton.setInteractive({ useHandCursor: true });
+
+		this.logoSubtext = this.add
+			.text(screenCenterX, 168, 'jump high with your Twitch chat', {
+				color: SUBTEXT_COLOR,
+				fontSize: '12px',
+				fontFamily: 'arial',
+			})
+			.setOrigin(0.5);
 
 		this.returnKey = this.input.keyboard.addKey(
 			Phaser.Input.Keyboard.KeyCodes.ENTER
@@ -34,12 +73,11 @@ export default class StartScreen extends Phaser.Scene {
 
 		this.returnKey.on('down', () => this.submit());
 		this.playButton.on('pointerdown', () => this.submit());
-
-		this.status.setColor('#ffffff');
 	}
 
 	async connectToChat(streamerHandle) {
 		let connectionError = false;
+		this.status.setColor(SUBTEXT_COLOR);
 		const opts = {
 			identity: {
 				username: 'brothersgettingbetter',
@@ -58,7 +96,7 @@ export default class StartScreen extends Phaser.Scene {
 		this.client = new tmi.client(opts);
 
 		try {
-			this.status.text = 'Loading';
+			this.status.text = 'Connecting to stream...';
 			await this.client.connect();
 			const res = await this.client.join(streamerHandle);
 			if (res) {
@@ -67,7 +105,8 @@ export default class StartScreen extends Phaser.Scene {
 			}
 		} catch (error) {
 			console.log(error);
-			this.status.text = `That streamer isn't live. Try again`;
+			this.status.setColor(ERROR_COLOR);
+			this.status.text = `That streamer isn't live. Try another`;
 		}
 	}
 
