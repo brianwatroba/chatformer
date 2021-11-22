bb5ed2767cad925c9b7e63d31d564ee52201e8d8
brian watroba <brianwatroba@gmail.com>
2021-11-22 08:33:47 -0500

    changed port to 4000 for new mac

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
 
 // app.get('/', function (req, res) {
