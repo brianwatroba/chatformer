f57576dd68220b37a5263049b86002aab06096f6
brian watroba <brianwatroba@gmail.com>
2021-12-07 18:41:44 -0500

    removed unused routes

--- server.js
+++ server.js
@@ -18,16 +18,3 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
-
-app.post("/connect", async (req, res) => {
-    console.log(req.body);
-    res.status(200);
-});
-
-app.get("/connect", async (req, res) => {
-    res.send("Worked");
-});
-
-app.get("/", (req, res) => {
-    res.redirect("https://www.google.com");
-});
