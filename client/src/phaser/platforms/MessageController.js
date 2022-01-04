99549cbfd5e3932e2e6bb8c080ba9c983a4d6fa4
Hong Jeon <hongj77@gmail.com>
2022-01-03 22:58:34 -0500

    render display names as part of message

--- client/src/phaser/platforms/MessageController.js
+++ client/src/phaser/platforms/MessageController.js
@@ -1,47 +1,5 @@
 import Phaser from 'phaser';
-
-class MessagePlatform extends Phaser.GameObjects.Text {
-    BASE_MOVE_SPEED = 50;
-    MOVE_SPEED_RANGE = 200;
-    direction = 1; //1 is right, -1 is left
-    initialized = false;
-
-    constructor(scene, x, y, text, playerY) {
-        var style = {
-            fontSize: '26px',
-            fill: '#FFFFFF',
-        };
-        super(scene, x, y, text, style); // x,y is blank here?
-        this.playerY = playerY
-    }
-
-    update() {
-        console.log('messageplatform update()')
-    }
-
-    setUp() {
-        this.direction = Math.random() > 0.5 ? 1 : -1;
-
-        if (this.body.width < 200) {
-            this.setColor('#0000FF');
-        }
-
-        this.setX(this.direction > 0 ? -300 - this.body.width : 500);
-        this.setY(this.playerY + 200 - 800 * Math.random())
-
-        this.body.setVelocityX(
-            this.direction *
-            (this.BASE_MOVE_SPEED + Math.random() * this.MOVE_SPEED_RANGE)
-        );
-        this.body.setAllowGravity(false);
-        this.body.setImmovable(true);
-        this.body.setFriction(1);
-        this.body.checkCollision.down = false;
-
-        this.setOrigin(0);
-    }
-}
-
+import MessagePlatform from './MessagePlatform';
 export class MessageController {
     constructor(scene, client) {
         this.scene = scene
@@ -78,7 +36,7 @@ export class MessageController {
     }
 
     _ingestMessage(message, player) {
-        const messagePlatform = new MessagePlatform(this.scene, 0, 0, message.message, player.y)
+        const messagePlatform = new MessagePlatform(this.scene, 0, 0, message, player.y)
         this.group.add(messagePlatform, true)
     }
 }
